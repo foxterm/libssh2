458320fb1a35c2231b990737be404d2887ff42ea
费三量 <feisanliang@feisanliangdeMac-mini.local>
2026-08-25 22:00:34 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "libssh2",
     platforms: [
         .macOS(.v14),
-        .iOS(.v17),
+        .iOS(.v16),
     ],
     products: [
         .library(name: "CSSH2", targets: ["CSSH2"])
